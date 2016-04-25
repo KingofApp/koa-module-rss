6f27b7707537263d95522ce0a109b7d5f00b4714
José Vicente Sogorb <thesentinel@kingofapp.com>
2016-04-25 18:37:15 +0200

    Replaced google feed api by yahoo query api

--- controller.js
+++ controller.js
@@ -1,26 +1,61 @@
-angular
-  .controller('rssCtrl', rssCtrl);
+(function(){
+  'use strict';
 
-rssCtrl.$inject = ['$scope', '$http', '$location', 'structureService', '$filter'];
+  angular
+    .controller('rssCtrl', rssCtrl);
 
-function rssCtrl($scope, $http, $location, structureService, $filter) {
-  //Register upper level modules
-  structureService.registerModule($location, $scope, 'rss');
+  rssCtrl.$inject = ['$scope', '$http', '$location', 'structureService', '$filter'];
 
-  $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load', {
-      params: {
-        'v': '1.0',
-        'q': $scope.rss.modulescope.feed,
-        'callback': 'JSON_CALLBACK'
-      }
-    })
-    .success(function(data) {
-      $scope.rssTitle = data.responseData.feed.title;
-      $scope.rssUrl = data.responseData.feed.feedUrl;
-      $scope.rssSiteUrl = data.responseData.feed.link;
-      $scope.entries = data.responseData.feed.entries;
-    })
-    .error(function(data) {
-      $scope.rss.message = $filter('translate')('rss.feed.error');
+  function rssCtrl($scope, $http, $location, structureService, $filter) {
+    //Register upper level modules
+    structureService.registerModule($location, $scope, 'rss');
+
+    $scope.ready = false;
+
+    $scope.max    = 0;
+    $scope.active = 0;
+
+    var format = '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK';
+    var query  = 'select * from xml where url = "'+ $scope.rss.modulescope.feed + '"';
+    var url    = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + format;
+
+    $http.jsonp(url).success(function(json) {
+      $scope.itemsY = json.query.results.rss.channel.item;
+      angular.forEach($scope.itemsY, function(post, key){
+        $scope.itemsY[key].desc = getDescription($scope.itemsY[key]);
+
+        if(post.thumbnail && post.thumbnail.length > 0){
+          $scope.itemsY[key].image = post.thumbnail[0].url;
+        }
+        else{
+          $scope.max++;
+          $scope.active++;
+          $http.get($scope.itemsY[key].link).success(function(data){
+            var images = data.match(/<meta.*property="og:image".*>/gmi)  ||
+                         data.match(/<meta.*name="twitter:image".*>/gmi) ||
+                         data.match(/<img[^>]*>/gmi);
+
+            for(var i=0; i<images.length; i++){
+              images[i] = images[i].match(/(src=")([^"]*)/gmi)[0].replace('src="','');
+            }
+            $scope.itemsY[key].image = images[$scope.rss.modulescope.image||0];
+            $scope.active--;
+          });
+        }
+      });
+
     });
-}
+
+    $scope.$on("koaAppRendered", function() {
+      console.log("rendering");
+      if($scope.active === 0) $scope.ready = true;
+    });
+  }
+
+  function getDescription(item){
+    return item.description.replace(/<[^>]*>/gmi, "")
+                           .replace(/read more/gmi, "")
+                           .replace(/leer mas/gmi, "");
+  }
+
+}());
