a774b37ba448ed7da698dbf72e97548a4ccb5289
José Vicente Sogorb <thesentinel@kingofapp.com>
2016-04-27 14:34:43 +0200

    Adding support for feed structure

--- controller.js
+++ controller.js
@@ -19,44 +19,53 @@
     var query  = 'select * from xml where url = "'+ $scope.rss.modulescope.feed + '"';
     var url    = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + format;
 
-    $http.jsonp(url).success(function(json) {
-      if (json && json.query && json.query.results){
-
-        $scope.itemsY = (json.query.results.rss) ? json.query.results.rss.channel.item :
-                                                   json.query.results.RDF.item;
-        angular.forEach($scope.itemsY, function(post, key){
-          $scope.itemsY[key].desc = getDescription($scope.itemsY[key]);
-
-          if(post.thumbnail && post.thumbnail.length > 0){
-            $scope.itemsY[key].image = post.thumbnail[0].url;
-          }
-          else{
-            $scope.max++;
-            $scope.active++;
-            $http.get($scope.itemsY[key].link)
-            .success(function(data){
-              var images = getImages(data);
-              for(var i=0; i<images.length; i++){
-                images[i] = images[i].match(/(src=")([^"]*)/gmi)[0].replace('src="','');
-              }
-              $scope.itemsY[key].image = images[$scope.rss.modulescope.image||0];
-              $scope.active--;
-            })
-            .error(function(){
-              $scope.active--;
-            });
-          }
-        });
-      }
+    $http.jsonp(url)
+    .success(function(json) {
+      if (!(json && json.query && json.query.results)) $scope.error = true;
       else{
-        $scope.error = true;
+
+        $scope.itemsY = (json.query.results.rss)  ? json.query.results.rss.channel.item :
+                        (json.query.results.RDF)  ? json.query.results.RDF.item         :
+                        (json.query.results.feed) ? json.query.results.feed.entry       :
+                                                    null;
+
+        if(!$scope.itemsY) $scope.error = true;
+        else               formatFeed();
+
       }
-
-
     })
     .error(function(error){
       $scope.error = true;
     });
+
+    function formatFeed(){
+      angular.forEach($scope.itemsY, function(post, key){
+        $scope.itemsY[key].desc = getDescription($scope.itemsY[key]);
+
+        if(post.thumbnail && post.thumbnail.length > 0){
+          $scope.itemsY[key].image = post.thumbnail[0].url;
+        }
+        else{
+          $scope.max++;
+          $scope.active++;
+          var link = $scope.itemsY[key].link.href || $scope.itemsY[key].link;
+          $http.get(link)
+          .success(function(data){
+            var images = getImages(data);
+            for(var i=0; i<images.length; i++){
+              images[i] = images[i].match(/(src=")([^"]*)/gmi)[0].replace('src="','');
+            }
+            $scope.itemsY[key].image = images[$scope.rss.modulescope.image||0];
+            $scope.active--;
+          })
+          .error(function(){
+            $scope.active--;
+          });
+        }
+      });
+
+
+  }
 
     $scope.$on("koaAppRendered", function() {
       console.log("rendering");
@@ -65,9 +74,14 @@
   }
 
   function getDescription(item){
-    return item.description.replace(/<[^>]*>/gmi, "")
-                           .replace(/read more/gmi, "")
-                           .replace(/leer mas/gmi, "");
+    var text = (item.description) ? item.description :
+               (item.content)     ? item.content.content :
+                                    null;
+                                    
+    return (!text) ? null : text.replace(/<[^>]*>/gmi, "")
+                                .replace(/read more/gmi, "")
+                                .replace(/leer mas/gmi, "")
+                                .trim();
   }
 
   function getImages(data){
